49ef6d9012cad6b91d508f5740f9352e49b542d5
aitorres2 <143133091+aitorres2@users.noreply.github.com>
2024-04-26 19:47:34 -0400

    Add files via upload

---
+++ RNA_seq_Clostridium_HGF2/GSEA/3_HGF2Con_late_vs_HGF2_late/gsea_double_5.R
@@ -0,0 +1,128 @@
+library(tidyverse)
+library(ggplot2)
+library(gridExtra)
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(ggrepel)
+library(tidyverse)
+library(gridExtra)
+library(ggrepel)
+library(grid)  # Asegurarse de incluir esta librería
+
+setwd("/home/alexis/Desktop/RESULTADOS_FINALES_ERGO/kallisto_TRABAJADO/HGF2/descargados_ERGO_deseq2/GSEA/3_HGF2Con_late_vs_HGF2_late")
+
+# Carga tus datos
+upregulated_ergo <- read_csv("gage (ergo): Consortium_Late - HGF2_Late upregulated.csv")
+downregulated_ergo <- read_csv("gage (ergo): Consortium_Late - HGF2_Late downregulated.csv")
+
+# Reemplazar '-' por '_' en la columna 'Name'
+upregulated_ergo <- upregulated_ergo %>% mutate(Name = str_replace_all(Name, "-", "_"))
+downregulated_ergo <- downregulated_ergo %>% mutate(Name = str_replace_all(Name, "-", "_"))
+downregulated_ergo$Name
+# Filtrar y remover duplicados basándose en la columna 'Name'
+upregulated_ergo_unique <- upregulated_ergo %>% distinct(Name, .keep_all = TRUE)
+downregulated_ergo_unique <- downregulated_ergo %>% distinct(Name, .keep_all = TRUE)
+
+# Filtrar los datos
+umbral_p_valor <- 0.05
+umbral_q_valor <- 0.05
+upregulated_filtered <- upregulated_ergo_unique %>% filter(`P Value` <= umbral_p_valor, `Q Value` <= umbral_q_valor)
+downregulated_filtered <- downregulated_ergo_unique %>% filter(`P Value` <= umbral_p_valor, `Q Value` <= umbral_q_valor)
+
+upregulated_filtered_clean$Name
+downregulated_filtered_clean$Name
+
+# Categorías a excluir
+categorias_excluir <- c("PRPPGLNIMP.ANA", "TAT_pathway_secreted_proteins", "TAT_pathway",
+                        "Bacterial_Endogenous_Protein_Quality_Control_and_Degradation",
+                        "Secretion", "Pyrimidine_salvage_pathways", "Sec_Dependent_Secretion_Stages",
+                        "Photosynthesis_in_cyanobacteria", "Heteropolysaccharide_biosynthesis",
+                        "Cytosolic_reactions_of_nitrogen_metabolism",
+                        "Electron_transport_in_cyanobacterial_tylakoid_membrane",
+                        "Sugar_precursors_of_streptococcal_capsular_and_exopolysaccharides",
+                        "Purine_salvage_pathways", "Bacterial_Translation_Factors",
+                        "Photosynthesis_and_respiration_in_cyanobacteria", "Bacterial_Photosynthesis",
+                        "Reserve_carbohydrate_biosynthesis", "Bacterial_Protein_Fate",
+                        "Bacterial_aerobic_respiration_proton_transport_and_oxidative_phosphorylation",
+                        "Bacterial_respiration_proton_transport_and_oxidative_phosphorylation", "Bacterial_Excision_Repair",
+                        "Glycolysis_versions_and_bypasses", "Bacterial_Ribosomal_Complex", "Bacterial_sporulation_transcription_regulators",
+                        "Polyketides_type_1", "Polyketides_type1_modular", "Enterobacteriaphage",
+                        "D_glucosamine_uptake_mannose_PTS_forming_glucosamine_6_phosphate", "Recombinational_Repair_RER",
+                        "D_mannosamine_uptake_mannose_PTS_forming_mannosamine_6_phosphate", "Biosynthesis_of_iron_cofactors",
+                        "N_acetylmannosamine_uptake_mannose_PTS_forming_ManNAc_6_phosphate", "Biosynthesis_of_nucleotide_sugar_precursors",
+                        "N_acetylglucosamine_uptake_mannose_PTS_forming_GlcNAc_6_phosphate", "Bacterial_Chromosomal_Replication_Initiation",
+                        "Teichoic_lipoteichoic_and_teichuronic_acid_biosynthesis", "Enterobacteria_phage_synthesis_proteins",
+                        "Bacterial_membrane_attached_embedded_dehydrogenases_and_oxidoreductases_anaerobic_respiration",
+                        "Bacterial_electron_transport_flavoprotein_quinone_oxidoreductases", "Biosynthesis_of_sulfur_containing_coenzymes",
+                        "UDP_N_acetylmuramoyl_oligo(depsi)peptide_biosynthesis", "Nucleotide_Excision_Repair_NER",
+                        "SOS_Error_Prone_Repair_SOS", "Bacterial_Miscellaneous_Translation_Proteins", "Virulence",
+                        "Antibiotic_class", "Bacterial_Chromatin_Architecture", "One_carbon_metabolism", "Gram_positive_sporulation_regulation",
+                        "Gram_positive_sporulation_stage_V", "Gram_positive_sporulation_stage_IV", "Sporulation_stages",
+                        "Proton_motive_cycle", "Bacterial_LSU", "Bacterial_SSU", "Anaerobic_respiratory_chain_proton_transport_and_oxidative_phosphorylation")
+
+
+# Filtrar y eliminar las categorías seleccionadas
+upregulated_filtered_clean <- upregulated_filtered %>% 
+  filter(!Name %in% categorias_excluir) %>%
+  arrange(`Q Value`)  # Orden ascendente para upregulated
+
+downregulated_filtered_clean <- downregulated_filtered %>% 
+  filter(!Name %in% categorias_excluir) %>%
+  arrange(`Q Value`)  # Orden descendente para downregulated
+
+# Cambiar el valor específico en el vector
+upregulated_filtered_clean$Name[upregulated_filtered_clean$Name == "D_glucose_uptake_mannose_PTS_forming_glucose_6_phosphate"] <- "D_glucose_uptake_mannose_PTS"
+upregulated_filtered_clean$Name[upregulated_filtered_clean$Name == "D_mannose_uptake_mannose_PTS_forming_mannose_6_phosphate"] <- "D_mannose_uptake_mannose_PTS"
+
+# Verificar los cambios
+upregulated_filtered_clean$Name
+downregulated_filtered_clean$Name
+
+library(scales)  # Para usar la función trans_new para transformaciones logarítmicas
+
+# Colores para la escala
+color_low <- "#E41A1C"  # Un tono pastel claro de rojo
+color_high <- "#377EB8"  # Un tono pastel claro de azul
+
+# Función para crear un gráfico con barras ordenadas por Q Value de forma ascendente
+create_plot <- function(data, title) {
+  # Reordena los datos en función del Q Value de forma ascendente
+  data <- data %>%
+    mutate(Name = reorder(Name, `Q Value`, FUN = function(x) -max(abs(x))))
+  
+  ggplot(data, aes(x = Name, y = `Stat Mean`, fill = `Q Value`)) +
+    geom_bar(stat = "identity", width = 0.7) +
+    coord_flip() +
+    scale_fill_gradientn(colors = c(color_low, color_high),
+                         trans = 'log10',  # Aplicar transformación logarítmica a la escala de colores
+                         na.value = "grey50",  # Color para los valores NA
+                         guide = guide_colourbar(title.position = "top", title.hjust = 0.5,
+                                                 label.theme = element_text(size = 8))) +
+    labs(title = title, x = "", y = "Stat Mean", fill = "Log10(Q-Value)") +
+    theme_minimal() +
+    theme(
+      text = element_text(face = "bold"),
+      axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"),
+      axis.text.y = element_text(size = 16, face = "bold"),
+      axis.title = element_text(face = "bold"),
+      plot.title = element_text(size = 14, face = "bold", hjust = 0.5),
+      legend.title = element_text(face = "bold"),
+      legend.text = element_text(face = "bold"),
+      plot.margin = margin(t = 1, r = 1, b = 1, l = 1, unit = "cm")
+    )
+}
+
+# Aplicar la función create_plot a tus datos filtrados y limpios
+upregulated_plot <- create_plot(upregulated_filtered_clean, "Upregulated genes")
+downregulated_plot <- create_plot(downregulated_filtered_clean, "Downregulated genes")
+
+# Organizar en dos columnas con el mismo ancho de barras y tamaño de texto y agregar título principal
+titulo_principal <- "HGF2_Consortium_Late_vs_HGF2_Single_Late"
+combined_plot <- grid.arrange(upregulated_plot, downregulated_plot, ncol = 2,
+                              top = textGrob(titulo_principal, gp = gpar(fontsize = 20, fontface = "bold")))
+
+# Guardar la imagen
+ggsave("combined_ergo_plot_3.png", combined_plot, width = 23, height = 15, dpi = 600)
+
---
+++ RNA_seq_Clostridium_HGF2/PCA_HCA/HGF2_PCA_RLOG_ERGO.R
@@ -0,0 +1,367 @@
+library(tidyverse)
+library(readxl)
+library("ggplot2")
+library("dplyr")
+library("tidyr")
+library("readr")
+library("purrr")
+library("tibble")
+library("stringr")
+library("forcats")
+library(stringr)
+library("pheatmap")
+library("grid")
+library(data.table)
+library("writexl")
+library(ggplot2)
+library(matrixStats)
+library(reshape2)
+library(grid)
+library(gridExtra)
+library(stringr)
+library(DESeq2)
+library(apeglm)
+library(ComplexHeatmap)
+library(circlize)
+library(RColorBrewer)
+library(PoiClaClu)
+library(apeglm)
+library(purrr)
+library(readr)
+library(pheatmap)
+library(clusterProfiler)
+library(gridExtra)
+library(cowplot)
+library(ggalt)
+library(ggforce)
+
+setwd("/home/alexis/Desktop/RESULTADOS_FINALES_ERGO/kallisto_TRABAJADO/HGF2/descargados_ERGO_deseq2/PCA")
+
+counts <- read.csv2("counts_HGF2_only_kallisto.csv",  row.names = 1, header = TRUE, sep=",")
+# Verificar la estructura de la tabla importada
+str(counts)
+
+min_count <- 10 # Puedes ajustar este número según tus necesidades.
+transformed_counts <- counts %>%
+  replace(is.na(.), 0) %>%
+  filter(rowSums(.) >= min_count)
+
+# Crear un vector que represente las muestras en el orden correcto
+HGF2_samples <- c("Consortium_Early_R1", "Consortium_Early_R2", "Consortium_Early_R3", 
+                  "Consortium_Late_R1", "Consortium_Late_R2", "Consortium_Late_R3", 
+                  "HGF2_Early_R1", "HGF2_Early_R2", "HGF2_Early_R3", 
+                  "HGF2_Late_R1", "HGF2_Late_R2", "HGF2_Late_R3")
+
+# Crear un vector que represente los grupos correspondientes a cada muestra
+HGF2_groups <- c("Consortium_Early", "Consortium_Early", "Consortium_Early", 
+                 "Consortium_Late", "Consortium_Late", "Consortium_Late", 
+                 "HGF2_Early", "HGF2_Early", "HGF2_Early", 
+                 "HGF2_Late", "HGF2_Late", "HGF2_Late")
+
+
+# Crear el objeto colData
+HGF2_colData <- data.frame(exp = as.factor(HGF2_groups),
+                           row.names = HGF2_samples)
+HGF2_colData
+
+# Counts transformados de HGF2
+HGF2_all_counts <- transformed_counts %>%
+  filter(rowSums(.) >= 10)
+# Verificar la estructura de la tabla filtrada
+str(HGF2_all_counts)
+
+# Verificar los nombres de fila y columna en HGF2_all_counts
+rownames(HGF2_all_counts)
+colnames(HGF2_all_counts)
+
+# Verificar los nombres de fila y columna en HGF2_colData
+rownames(HGF2_colData)
+colnames(HGF2_colData)
+
+# Obtener los nuevos nombres de las columnas desde las filas de HGF2_colData
+new_column_names <- rownames(HGF2_colData)
+
+# Asignar los nuevos nombres de las columnas a HGF2_all_counts
+colnames(HGF2_all_counts) <- new_column_names
+
+# Crear el objeto DESeqDataSet utilizando el countData y el colData
+design_HGF2_all_counts <- DESeqDataSetFromMatrix(countData = HGF2_all_counts,
+                                                 colData = HGF2_colData,
+                                                 design = ~ exp)
+
+# Normalizar counts, librearia en cantidad de reads (siempre se deben normalizar los datos de rna-seq); esta normalizacion es unica de deseq
+dds_HGF2_all_counts <- estimateSizeFactors(design_HGF2_all_counts)
+# View
+sizeFactors(dds_HGF2_all_counts)
+# Extract normalized counts
+counts_dds_HGF2_all_counts <- counts(dds_HGF2_all_counts, normalized = TRUE)
+
+# Realizar la transformación rlog considerando las condiciones experimentales
+rld_dds_HGF2_all_counts <- rlog(dds_HGF2_all_counts, blind = FALSE)
+HGF2_all_pca <- plotPCA(rld_dds_HGF2_all_counts, intgroup = "exp")
+HGF2_all_pca
+# Realizar la transformación rlog considerando las condiciones experimentales
+#rld_dds_HGF2_all_counts <- rlog(dds_HGF2_all_counts, blind = TRUE)
+
+rld_mat_dds_HGF2_all_counts <- assay(rld_dds_HGF2_all_counts) 
+rld_cor_mat_dds_HGF2_all_counts <- cor(rld_mat_dds_HGF2_all_counts, method = "spearman")
+#rld_cor_mat_dds_HGF2_all_counts <- cor(rld_mat_dds_HGF2_all_counts) # pearson correlation
+
+p1 <- pheatmap(rld_cor_mat_dds_HGF2_all_counts,
+               clustering_distance_cols = "euclidean",
+               clustering_distance_rows = "euclidean",
+               clustering_method = "complete",
+               legend = TRUE) # Añadir título a la leyenda
+p1
+
+#png("HGF2_pheatmap_pre_filt.png", bg = "white", width = 20, height = 15, units = 'cm', res = 500)
+#pheatmap(rld_cor_mat_dds_HGF2_all_counts)
+#dev.off()
+
+# CUales exactamente la replica que esta molestando:
+# Generar el gráfico PCA y utilizar "condition" como intgroup
+HGF2_all_pcaData <- plotPCA(rld_dds_HGF2_all_counts, intgroup = "exp", returnData = TRUE)
+
+# Agregar columna "replicate" a partir de los nombres de las muestras
+HGF2_all_pcaData$replicate <- HGF2_all_pcaData$name
+
+# Crear columna de replicado solo con valores 'replicate1', 'replicate2', 'replicate3'
+HGF2_all_pcaData$replicate <- paste0('Replicate', substr(HGF2_all_pcaData$replicate, nchar(HGF2_all_pcaData$replicate), nchar(HGF2_all_pcaData$replicate)))
+
+# Calcular el porcentaje de varianza explicada por cada componente
+percentVar <- round(100 * attr(HGF2_all_pcaData, "percentVar"), 2)
+
+# Asignar formas automáticamente a las categorías únicas
+shape_palette <- c(15, 16, 17)  # ahora tenemos 4 formas
+
+# Definir unique_exp_categories
+unique_exp_categories <- unique(HGF2_all_pcaData$exp)
+
+# Asignar colores automáticamente a las categorías únicas
+color_palette <- colorRampPalette(c("#1f78b4", "#33a02c", "#e31a1c", "#9370DB"))(length(unique_exp_categories))
+
+#HGF2_all_pcaData$group <- HGF2_all_pcaData$exp # Crea un nuevo factor que agrupe los datos
+
+# Asignar colores automáticamente a las categorías únicas
+color_palette <- colorRampPalette(c("#1f78b4", "#33a02c", "#e31a1c", "#9370DB"))(length(unique_exp_categories))
+
+p2 <- ggplot(HGF2_all_pcaData, aes(x = PC1, y = PC2, color = exp)) +
+  geom_point(aes(shape = replicate), size = 4) +
+  geom_mark_ellipse(aes(fill = exp), 
+                    asp_ratio = 1, 
+                    expand = 0.02,  # Valor más pequeño para hacer la elipse más ajustada
+                    alpha = 0.2,
+                    show.legend = FALSE) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(colour = "black"),
+    plot.title = element_text(size = 15, face = "bold", hjust = 0.5)
+  ) +
+  scale_color_manual("Experiment", values = color_palette) +
+  scale_shape_manual("Replicates", values = shape_palette) +
+  guides(
+    color = guide_legend(order = 1),
+    shape = guide_legend(order = 2)
+  ) +
+  scale_fill_manual("Experiment", values = color_palette)
+
+# Guardar gráfico PCA previo a la remoción de outliers
+#if (!dir.exists("results")) {
+#  dir.create("results")
+#}
+
+p2
+
+#ggsave("HGF2_PCA_2_pre_filt.png", p2, bg = "white", dpi = 500, 
+#       width = 30, height = 20, units = 'cm')
+
+#########################################################################################################################################################################3
+# Eliminar muestra
+colnames(transformed_counts)
+min_count <- 10 # Puedes ajustar este número según tus necesidades.
+transformed_counts <- counts %>%
+  select(-Consortium_Early_R3, -Consortium_Late_R1) %>%
+  replace(is.na(.), 0) %>%
+  filter(rowSums(.) >= min_count)
+colnames(transformed_counts)
+
+# Crear un vector que represente las muestras en el orden correcto
+HGF2_samples <- c("Consortium_Early_R1", "Consortium_Early_R2",
+                  "Consortium_Late_R2", "Consortium_Late_R3", 
+                  "HGF2_Early_R1", "HGF2_Early_R2", "HGF2_Early_R3",
+                  "HGF2_Late_R1", "HGF2_Late_R2", "HGF2_Late_R3")
+
+# Crear un vector que represente los grupos correspondientes a cada muestra
+HGF2_groups <- c("Consortium_Early", "Consortium_Early",
+                 "Consortium_Late", "Consortium_Late",
+                 "HGF2_Early", "HGF2_Early", "HGF2_Early",
+                 "HGF2_Late", "HGF2_Late", "HGF2_Late")
+
+# Crear el objeto colData
+HGF2_colData <- data.frame(exp = as.factor(HGF2_groups),
+                           row.names = HGF2_samples)
+HGF2_colData
+
+# Counts transformados de HGF2
+HGF2_all_counts <- transformed_counts %>%
+  filter(rowSums(.) >= 10)
+# Verificar la estructura de la tabla filtrada
+str(HGF2_all_counts)
+
+# Verificar los nombres de fila y columna en HGF2_all_counts
+rownames(HGF2_all_counts)
+colnames(HGF2_all_counts)
+
+# Verificar los nombres de fila y columna en HGF2_colData
+rownames(HGF2_colData)
+colnames(HGF2_colData)
+
+# Obtener los nuevos nombres de las columnas desde las filas de HGF2_colData
+new_column_names <- rownames(HGF2_colData)
+
+# Asignar los nuevos nombres de las columnas a HGF2_all_counts
+colnames(HGF2_all_counts) <- new_column_names
+
+# Crear el objeto DESeqDataSet utilizando el countData y el colData
+design_HGF2_all_counts <- DESeqDataSetFromMatrix(countData = HGF2_all_counts,
+                                                 colData = HGF2_colData,
+                                                 design = ~ exp)
+
+# Normalizar counts, librearia en cantidad de reads (siempre se deben normalizar los datos de rna-seq); esta normalizacion es unica de deseq
+dds_HGF2_all_counts <- estimateSizeFactors(design_HGF2_all_counts)
+# View
+sizeFactors(dds_HGF2_all_counts)
+# Extract normalized counts
+counts_dds_HGF2_all_counts <- counts(dds_HGF2_all_counts, normalized = TRUE)
+
+# Realizar la transformación rlog considerando las condiciones experimentales
+rld_dds_HGF2_all_counts <- rlog(dds_HGF2_all_counts, blind = FALSE)
+HGF2_all_pca <- plotPCA(rld_dds_HGF2_all_counts, intgroup = "exp")
+HGF2_all_pca
+# Realizar la transformación rlog considerando las condiciones experimentales
+#rld_dds_HGF2_all_counts <- rlog(dds_HGF2_all_counts, blind = TRUE)
+
+rld_mat_dds_HGF2_all_counts <- assay(rld_dds_HGF2_all_counts) 
+rld_cor_mat_dds_HGF2_all_counts <- cor(rld_mat_dds_HGF2_all_counts, method = "spearman")
+p3 <- pheatmap(rld_cor_mat_dds_HGF2_all_counts,
+               clustering_distance_cols = "euclidean", clustering_distance_rows = "euclidean",
+               clustering_method = "complete")
+p3
+
+#png("HGF2_pheatmap_post_filt.png", bg = "white", width = 30, height = 20, units = 'cm', res = 500)
+#pheatmap(rld_cor_mat_dds_HGF2_all_counts)
+#dev.off()
+
+##########################################
+# CUales exactamente la replica que esta molestando:
+# Generar el gráfico PCA y utilizar "condition" como intgroup
+HGF2_all_pcaData <- plotPCA(rld_dds_HGF2_all_counts, intgroup = "exp", returnData = TRUE)
+
+# Agregar columna "replicate" a partir de los nombres de las muestras
+HGF2_all_pcaData$replicate <- HGF2_all_pcaData$name
+
+# Crear columna de replicado solo con valores 'replicate1', 'replicate2', 'replicate3'
+HGF2_all_pcaData$replicate <- paste0('Replicate', substr(HGF2_all_pcaData$replicate, nchar(HGF2_all_pcaData$replicate), nchar(HGF2_all_pcaData$replicate)))
+
+# Calcular el porcentaje de varianza explicada por cada componente
+percentVar <- round(100 * attr(HGF2_all_pcaData, "percentVar"), 2)
+
+# Asignar formas automáticamente a las categorías únicas
+shape_palette <- c(15, 16, 17)  # ahora tenemos 4 formas
+
+# Definir unique_exp_categories
+unique_exp_categories <- unique(HGF2_all_pcaData$exp)
+
+# Asignar colores automáticamente a las categorías únicas
+color_palette <- colorRampPalette(c("#1f78b4", "#33a02c", "#e31a1c", "#9370DB"))(length(unique_exp_categories))
+
+#HGF2_all_pcaData$group <- HGF2_all_pcaData$exp # Crea un nuevo factor que agrupe los datos
+
+# Asignar colores automáticamente a las categorías únicas
+color_palette <- colorRampPalette(c("#1f78b4", "#33a02c", "#e31a1c", "#9370DB"))(length(unique_exp_categories))
+
+p4 <- ggplot(HGF2_all_pcaData, aes(x = PC1, y = PC2, color = exp)) +
+  geom_point(aes(shape = replicate), size = 4) +
+  geom_mark_ellipse(aes(fill = exp), 
+                    asp_ratio = 1, 
+                    expand = 0.04, 
+                    alpha = 0.2,
+                    show.legend = F) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(colour = "black"),
+    plot.title = element_text(size = 15, face = "bold", hjust = 0.5)
+  ) +
+  scale_color_manual("Experiment", values = color_palette) +
+  scale_shape_manual("Replicates", values = shape_palette) +
+  guides(
+    color = guide_legend(order = 1),
+    shape = guide_legend(order = 2)
+  ) +
+  scale_fill_manual("Experiment", values = color_palette)
+p4
+
+# Guardar gráfico PCA previo a la remoción de outliers
+#if (!dir.exists("results")) {
+#  dir.create("results")
+#}
+
+p4 
+
+#ggsave("HGF2_PCA_2_post_filt.png", ggsave_pca, bg = "white", dpi = 500, 
+#       width = 30, height = 20, units = 'cm')
+
+# Asegúrate de que las figuras p1, p2, p3, y p4 están correctamente generadas
+
+# Guardar cada heatmap como una imagen
+pheatmap_save <- function(p, filename) {
+  png(filename, width = 7, height = 7, units = 'in', res = 600)
+  print(p)
+  dev.off()
+}
+
+# Reemplaza 'p1', 'p2', 'p3', 'p4' con tus objetos de gráficos reales
+pheatmap_save(p1, "heatmap1.png")
+pheatmap_save(p2, "heatmap2.png")
+pheatmap_save(p3, "heatmap3.png")
+pheatmap_save(p4, "heatmap4.png")
+
+# Instalar y cargar las bibliotecas si aún no están instaladas
+if (!require("grid")) install.packages("grid")
+if (!require("gridExtra")) install.packages("gridExtra")
+if (!require("png")) install.packages("png")
+
+library(grid)
+library(gridExtra)
+library(png)
+
+# Leer las imágenes
+img1 <- readPNG("heatmap1.png")
+img2 <- readPNG("heatmap2.png")
+img3 <- readPNG("heatmap3.png")
+img4 <- readPNG("heatmap4.png")
+
+# Convertir imágenes en grobs
+grob1 <- rasterGrob(img1, interpolate = TRUE)
+grob2 <- rasterGrob(img2, interpolate = TRUE)
+grob3 <- rasterGrob(img3, interpolate = TRUE)
+grob4 <- rasterGrob(img4, interpolate = TRUE)
+
+# Combinar las imágenes
+combined_plot <- grid.arrange(
+  grob1, grob2,
+  grob3, grob4,
+  ncol = 2,
+  heights = c(1, 1) # Ajustar si es necesario
+)
+
+# Guardar la imagen combinada
+ggsave("combined_heatmaps_HGF2.png", combined_plot, width = 14, height = 14, units = 'in', dpi = 600)
+
+
