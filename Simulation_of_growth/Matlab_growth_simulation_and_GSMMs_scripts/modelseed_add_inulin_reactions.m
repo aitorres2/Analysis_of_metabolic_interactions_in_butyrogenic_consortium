4386b908dec716cac7af2efd952a270c443b0f9a
aitorres2 <143133091+aitorres2@users.noreply.github.com>
2024-05-04 22:42:18 -0400

    Update modelseed_add_inulin_reactions.m

--- Simulation_of_growth/Matlab_growth_simulation_and_GSMMs_scripts/modelseed_add_inulin_reactions.m
+++ Simulation_of_growth/Matlab_growth_simulation_and_GSMMs_scripts/modelseed_add_inulin_reactions.m
@@ -96,51 +96,13 @@ else
     disp(['Reaction ' oldReactionName ' not found in the model.']);
 end
 
-%% Establecer funcion objetivo biomasa
-objectiveAbbr_Bifidobacterium_animalis_lactis_PT33 = checkObjective(Bifidobacterium_animalis_lactis_PT33);
-objectiveAbbr_Clostridium_sp_7_2_43FAA = checkObjective(Clostridium_sp_7_2_43FAA);
-objectiveAbbr_Clostridium_sp_M62 = checkObjective(Clostridium_sp_M62);
-%%
-Bifidobacterium_animalis_lactis_PT33 = changeObjective(Bifidobacterium_animalis_lactis_PT33, 'bio1_biomass', 1);  % Set the biomass reaction coefficient to 1
-objectiveAbbr_Bifidobacterium_animalis_lactis_PT33 = checkObjective(Bifidobacterium_animalis_lactis_PT33);
-
-%%
-objectiveAbbr_Bifidobacterium_longum_PT8 = checkObjective(Bifidobacterium_longum_PT8);
-objectiveAbbr_Clostridium_innocuum_HGF2_Manual_Inulin = checkObjective(Clostridium_innocuum_HGF2_Manual_Inulin);
-%%
-% cpd00023 = L-Glutamate
-% cpd00281 = GABA
-% cpd00211 = butyrate
-% cpd00029 = acetate
-% cpd11602 = inulin
-% cpd00036 = succinate
-% cpd00047 = formate
-% cpd00159 = L-lactate
-% cpd00221 = D-lactate
-% cpd01022 = lactate
-% cpd00141 = propionate
-% cpd00363 = ethanol
-%     'EX_cpd02298_e0';... FOS
-%     'EX_cpd00027_e0';... % D-glucose
-%     'EX_cpd00082_e0';... % D-fructose
-%     'EX_cpd00076_e0';... % Sucrose
-    
-%%
-Bifidobacterium_longum_PT8 = changeObjective(Bifidobacterium_longum_PT8, 'bio1_biomass', 1);  % Set the biomass reaction coefficient to 1
-Clostridium_innocuum_HGF2_Manual_Inulin = changeObjective(Clostridium_innocuum_HGF2_Manual_Inulin, 'bio1_biomass', 1);  % Set the biomass reaction coefficient to 1
-
-%% refinar modelo de inulina: https://opencobra.github.io/cobratoolbox/latest/modules/reconstruction/refinement/index.html
-
-%% ESTA REACCION NO DEBERIA ESTA EN EL MODELO
-% buscar que reaccions el modelo no deberia tener o si deberia tener:
 %%
 surfNet(Bifidobacterium_animalis_lactis_PT33, "cpd00211[e0]"); % butyrate; no deberia tener %%%% no tiene
 surfNet(Bifidobacterium_animalis_lactis_PT33, "cpd11602[e0]"); % inulina; no deberia tener %%%% no tiene
-
 %%
-surfNet(Clostridium_sp_7_2_43FAA, "cpd00211[e0]"); % butyrate; si deberia tener %%%% si tiene
+surfNet(Clostridium_sp_7_2_43FAA, "cpd00211[e0]"); % butyrate; 
 %%
-surfNet(Clostridium_sp_M62, "cpd00211[e0]"); % butyrate; si deberia tener
+surfNet(Clostridium_sp_M62, "cpd00211[e0]"); % butyrate; 
 %%
 
 
@@ -219,8 +181,6 @@ Bacteroides_thetaiotaomicron_VPI_5482 = removeRxns(Bacteroides_thetaiotaomicron_
 Bacteroides_thetaiotaomicron_VPI_5482 = removeRxns(Bacteroides_thetaiotaomicron_VPI_5482, 'EX_cpd00211_e0');
 
 %%
-Lacticaseibacillus_paracasei_M38 = readCbModel('Lacticaseibacillus_paracasei_M38');
-%%
 surfNet(Lacticaseibacillus_paracasei_M38, "cpd00211[e0]"); % butirato %%%% no tiene este metabolito
 Lacticaseibacillus_paracasei_M38 = removeRxns(Lacticaseibacillus_paracasei_M38, 'rxn05683_c0');
 Lacticaseibacillus_paracasei_M38 = removeRxns(Lacticaseibacillus_paracasei_M38, 'EX_cpd00211_e0');
@@ -250,23 +210,6 @@ Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38,
 'cpd00001[c0] + cpd02298[c0]  -> cpd00076[c0] + cpd30321[c0]');
 Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn38360_e0',...
 '2 cpd00076[c0]  <=> cpd00190[c0] + cpd02298[c0]');
-%%
-% cpd00023 = L-Glutamate
-% cpd00281 = GABA
-% cpd00211 = butyrate
-% cpd00029 = acetate
-% cpd11602 = inulin
-% cpd00036 = succinate
-% cpd00047 = formate
-% cpd00159 = L-lactate
-% cpd00221 = D-lactate
-% cpd01022 = lactate
-% cpd00141 = propionate
-% cpd00363 = ethanol
-%     'EX_cpd02298_e0';... FOS
-%     'EX_cpd00027_e0';... % D-glucose
-%     'EX_cpd00082_e0';... % D-fructose
-%     'EX_cpd00076_e0';... % Sucrose
 
 surfNet(Bifidobacterium_animalis_lactis_PT33, "cpd00082[e0]"); % fructose %%%% no tiene este metabolito
 %%
@@ -282,23 +225,6 @@ surfNet(Clostridium_sp_M62, "cpd00082[e0]"); % butirato %%%% no tiene este metab
 surfNet(Bifidobacterium_animalis_lactis_PT33, "cpd00076[e0]"); % butirato %%%% no tiene este metabolito
 surfNet(Clostridium_sp_M62, "cpd00076[e0]"); % butirato %%%% no tiene este metabolito
 
-%% Seguimiento rutas de metabolitos
-% https://opencobra.github.io/cobratoolbox/latest/modules/analysis/exploration/index.html?highlight=biomassPrecursorCheck#src.analysis.exploration.biomassPrecursorCheck
-
-% biomassPrecursorCheck(Lacticaseibacillus_paracasei_M38) %la consola me imprime los metabolitos esencailes apra la biomasa pero q no los pouede producir el modelo
-
-% cpd00023 = L-Glutamate
-% cpd00281 = GABA
-% cpd00211 = butyrate
-% cpd00029 = acetate
-% cpd11602 = inulin
-% cpd00036 = succinate
-% cpd00047 = formate
-% cpd00159 = L-lactate
-% cpd00221 = D-lactate
-% cpd01022 = lactate
-% cpd00141 = propionate
-% cpd00363 = ethanol
 % 
 % surfNet(Lacticaseibacillus_paracasei_M38, "cpd00029[e0]");% cpd00029 = acetate
 % surfNet(Lacticaseibacillus_paracasei_M38, "cpd00036[e0]");% cpd00036 = succinate
@@ -329,28 +255,5 @@ surfNet(Clostridium_sp_M62, "cpd00076[e0]"); % butirato %%%% no tiene este metab
 % surfNet(Clostridium_sp_M62, "cpd00141[e0]");% cpd00141 = propionate
 % surfNet(Clostridium_sp_M62, "cpd00211[e0]");% cpd00211 = butyrate
 % surfNet(Clostridium_sp_M62, "cpd00363[e0]");% cpd00363 = ethanol
-% 
-% % Comparacion con modelos AGORA
-% 
-% Bifidobacterium_longum_DJO10A = readCbModel('Bifidobacterium_longum_DJO10A');
-% Lactobacillus_casei_ATCC_334 =readCbModel('Lactobacillus_casei_ATCC_334');
-% 
-% surfNet(Bifidobacterium_longum_DJO10A, "ac[e]");% cpd00029 = acetate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00036[e0]");% cpd00036 = succinate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00047[e0]");% cpd00047 = formate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00159[e0]");% cpd00159 = L-lactate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00221[e0]");% cpd00221 = D-lactate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd01022[e0]");% cpd01022 = lactate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00141[e0]");% cpd00141 = propionate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00141[e0]");% cpd00211 = butyrate
-% surfNet(Bifidobacterium_longum_DJO10A, "cpd00363[e0]");% cpd00363 = ethanol
-% 
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd00029[e0]");% cpd00029 = acetate
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd00036[e0]");% cpd00036 = succinate
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd00047[e0]");% cpd00047 = formate
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd00159[e0]");% cpd00159 = L-lactate
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd00221[e0]");% cpd00221 = D-lactate
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd01022[e0]");% pd01022 = lactate
-% surfNet(Lactobacillus_casei_ATCC_334, "cpd00141[e0]");% cpd00141 = propionate
 % surfNet(Lactobacillus_casei_ATCC_334, "cpd00211[e0]");% cpd00211 = butyrate
 % surfNet(Lactobacillus_casei_ATCC_334, "cpd00363[e0]");% cpd00363 = ethanol
