c14b7474a0984724f9befc01fb8540f99db46696
aitorres2 <143133091+aitorres2@users.noreply.github.com>
2024-05-04 21:56:35 -0400

    Update VMH_models_refinement.m

--- Simulation_of_growth/Matlab_growth_simulation_and_GSMMs_scripts/VMH_models_refinement.m
+++ Simulation_of_growth/Matlab_growth_simulation_and_GSMMs_scripts/VMH_models_refinement.m
@@ -1,6 +1,4 @@
-%% refinement of gapseq models for simulation on inulin
 % Change working directory if necessary
-% El medio de crecimiento es el izmb1 con acetato. 
 clearvars
 clc
 %%
@@ -15,7 +13,6 @@ Clostridium_sp_7_2_43FAA = readCbModel('Clostridium_sp_7_2_43FAA');
 Clostridium_sp_M62 = readCbModel('Clostridium_sp_M62');
 Clostridium_symbiosum_WAL_14673 = readCbModel('Clostridium_symbiosum_WAL_14673');
 Lacticaseibacillus_paracasei_M38 = readCbModel('Lacticaseibacillus_paracasei_M38');
-
 
 %%
 % Define the old reaction name and the new reaction name
@@ -104,93 +101,6 @@ end
 objectiveAbbr_Bifidobacterium_animalis_lactis_PT33 = checkObjective(Bifidobacterium_animalis_lactis_PT33);
 objectiveAbbr_Clostridium_sp_7_2_43FAA = checkObjective(Clostridium_sp_7_2_43FAA);
 objectiveAbbr_Clostridium_sp_M62 = checkObjective(Clostridium_sp_M62);
-%%
-Bifidobacterium_animalis_lactis_PT33 = changeObjective(Bifidobacterium_animalis_lactis_PT33, 'bio1_biomass', 1);  % Set the biomass reaction coefficient to 1
-objectiveAbbr_Bifidobacterium_animalis_lactis_PT33 = checkObjective(Bifidobacterium_animalis_lactis_PT33);
-
-%%
-objectiveAbbr_Bifidobacterium_longum_PT8 = checkObjective(Bifidobacterium_longum_PT8);
-objectiveAbbr_Clostridium_innocuum_HGF2_Manual_Inulin = checkObjective(Clostridium_innocuum_HGF2_Manual_Inulin);
-%%
-% cpd00023 = L-Glutamate
-% cpd00281 = GABA
-% cpd00211 = butyrate
-% cpd00029 = acetate
-% cpd11602 = inulin
-% cpd00036 = succinate
-% cpd00047 = formate
-% cpd00159 = L-lactate
-% cpd00221 = D-lactate
-% cpd01022 = lactate
-% cpd00141 = propionate
-% cpd00363 = ethanol
-%     'EX_cpd02298_e0';... FOS
-%     'EX_cpd00027_e0';... % D-glucose
-%     'EX_cpd00082_e0';... % D-fructose
-%     'EX_cpd00076_e0';... % Sucrose
-    
-%%
-Bifidobacterium_longum_PT8 = changeObjective(Bifidobacterium_longum_PT8, 'bio1_biomass', 1);  % Set the biomass reaction coefficient to 1
-Clostridium_innocuum_HGF2_Manual_Inulin = changeObjective(Clostridium_innocuum_HGF2_Manual_Inulin, 'bio1_biomass', 1);  % Set the biomass reaction coefficient to 1
-
-%% THIS REACTION SHOULD NOT BE IN THE MODEL
-% look for what reactions the model should not have or should have:
-%%
-surfNet(Bifidobacterium_animalis_lactis_PT33, "cpd00211[e0]"); % butyrate; no deberia tener %%%% no tiene
-surfNet(Bifidobacterium_animalis_lactis_PT33, "cpd11602[e0]"); % inulina; no deberia tener %%%% no tiene
-
-%%
-surfNet(Clostridium_sp_7_2_43FAA, "cpd00211[e0]"); % butyrate; si deberia tener %%%% si tiene
-%%
-surfNet(Clostridium_sp_M62, "cpd00211[e0]"); % butyrate; si deberia tener
-%%
-
-
-%%
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn05695_e0')
-%%
-% printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn23732_c0')
-
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn23732_c0')
-
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn28593_e0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn29776_e0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn00012_c0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn23732_c0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn23734_c0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn23739_c0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn23749_c0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn28566_e0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn29535_e0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn29739_e0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn37232_c0')
-%%
-clc
-printRxnFormula(Clostridium_innocuum_HGF2_Manual_Inulin, 'rxn38360_c0')
-%%
 
 %% Adding inulin and FOS modelseed reactions
 Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'EX_cpd11602_e0',...
@@ -199,8 +109,13 @@ Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'EX_cpd02298_
 'cpd02298[e0] <=>');
 
 %Reacción de transporte de inulina con protones: rxn05695
-Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn05695_e0',...
-'reactionFormula', 'cpd00067[c0] + cpd11602[c0] <=> cpd00067[e0] + cpd11602[e0]');
+% Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn05695_e0',...
+% 'reactionFormula', 'cpd00067[c0] + cpd11602[c0] <=> cpd00067[e0] + cpd11602[e0]');
+
+
+Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn28566_e0',...
+'reactionFormula', 'cpd02298[c0] <=> cpd02298[e0]');
+
 
 %Descomposición de 1F-beta-D-Fructosylsucrose (repetida, por lo que sólo
 %listo una): rxn23732 / rxn23733
@@ -217,8 +132,8 @@ Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn29776_e0'
 
 %Reacción de intercambio de protones con transporte de inulina (en ambas
 %direcciones): rxn29847
-Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn29847_e0',...
-'reactionFormula', 'cpd00067[e0] + cpd11602[c0] <=> cpd00067[c0] + cpd11602[e0]');
+% Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn29847_e0',...
+% 'reactionFormula', 'cpd00067[e0] + cpd11602[c0] <=> cpd00067[c0] + cpd11602[e0]');
 
 Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn00012_e0',...
 'reactionFormula', '2 cpd00076[e0]  <=> cpd00027[e0] + cpd02298[e0]');
@@ -226,8 +141,8 @@ Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn00012_e0'
 Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn23739_e0',...
 'reactionFormula', 'cpd00076[e0] + cpd02298[e0]  <=> cpd00190[e0] + cpd22431[e0]');
 
-% Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn23749_e0',...
-% 'reactionFormula', 'cpd00001[e0] + cpd02298[e0]  <=> cpd00076[e0] + cpd19102[e0]');
+Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn23749_e0',...
+'reactionFormula', 'cpd00001[e0] + cpd02298[e0]  <=> cpd00076[e0] + cpd19102[e0]');
 
 Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn29739_e0',...
 'cpd00001[e0] + cpd00002[e0] + cpd02298[c0] => cpd00008[e0] + cpd00009[e0] + cpd00067[e0] + cpd02298[e0]');
@@ -238,13 +153,6 @@ Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn37232_e0'
 Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'rxn38360_e0',...
 '2 cpd00076[e0]  <=> cpd00190[e0] + cpd02298[e0]');
 
-
-%% inulin consumption extracellular according to VHM
-% Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'INULINASEe_VMH',...
-% '25.0 cpd00001[e0] + cpd11602[e0] -> 25.0 cpd00082[e0] + cpd02298[e0]');
-% 
-% Clostridium_innocuum_HFG2 = addReaction(Clostridium_innocuum_HFG2, 'KESTOPTASEe_VMH',...
-% '4.0 cpd00001[e0] + cpd02298[e0] -> 4.0 cpd00082[e0] + cpd00027[e0]');
 	
 %%
 
@@ -255,8 +163,13 @@ Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38,
 'cpd02298[e0] <=>');
 
 %Reacción de transporte de inulina con protones: rxn05695
-Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn05695_e0',...
-'reactionFormula', 'cpd00067[c0] + cpd11602[c0] <=> cpd00067[e0] + cpd11602[e0]');
+% Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn05695_e0',...
+% 'reactionFormula', 'cpd00067[c0] + cpd11602[c0] <=> cpd00067[e0] + cpd11602[e0]');
+
+
+Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn28566_e0',...
+'reactionFormula', 'cpd02298[c0] <=> cpd02298[e0]');
+
 
 %Descomposición de 1F-beta-D-Fructosylsucrose (repetida, por lo que sólo
 %listo una): rxn23732 / rxn23733
@@ -271,16 +184,14 @@ Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38,
 Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn29776_e0',...
 'reactionFormula', 'cpd00001[e0] + cpd00002[e0] + cpd11602[c0] => cpd00008[e0] + cpd00009[e0] + cpd00067[e0] + cpd11602[e0]');
 
-%Reacción de intercambio de protones con transporte de inulina (en ambas
-%direcciones): rxn29847
-Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn29847_e0',...
-'reactionFormula', 'cpd00067[e0] + cpd11602[c0] <=> cpd00067[c0] + cpd11602[e0]');
-
 Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn00012_e0',...
 'reactionFormula', '2 cpd00076[e0]  <=> cpd00027[e0] + cpd02298[e0]');
 
 Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn23739_e0',...
 'reactionFormula', 'cpd00076[e0] + cpd02298[e0]  <=> cpd00190[e0] + cpd22431[e0]');
+
+Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn23749_e0',...
+'reactionFormula', 'cpd00001[e0] + cpd02298[e0]  <=> cpd00076[e0] + cpd19102[e0]');
 
 Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn29739_e0',...
 'cpd00001[e0] + cpd00002[e0] + cpd02298[c0] => cpd00008[e0] + cpd00009[e0] + cpd00067[e0] + cpd02298[e0]');
@@ -290,31 +201,4 @@ Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38,
 
 Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'rxn38360_e0',...
 '2 cpd00076[e0]  <=> cpd00190[e0] + cpd02298[e0]');
-
-
-
-% %% inulin consumption extracellular according to VHM
-% Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'INULINASEe_VMH',...
-% '25.0 cpd00001[e0] + cpd11602[e0] -> 25.0 cpd00082[e0] + cpd02298[e0]');
-% 
-% Lacticaseibacillus_paracasei_M38 = addReaction(Lacticaseibacillus_paracasei_M38, 'KESTOPTASEe_VMH',...
-% '4.0 cpd00001[e0] + cpd02298[e0] -> 4.0 cpd00082[e0] + cpd00027[e0]');
 	
-
-%%
-% cpd00023 = L-Glutamate
-% cpd00281 = GABA
-% cpd00211 = butyrate
-% cpd00029 = acetate
-% cpd11602 = inulin
-% cpd00036 = succinate
-% cpd00047 = formate
-% cpd00159 = L-lactate
-% cpd00221 = D-lactate
-% cpd01022 = lactate
-% cpd00141 = propionate
-% cpd00363 = ethanol
-%     'EX_cpd02298_e0';... FOS
-%     'EX_cpd00027_e0';... % D-glucose
-%     'EX_cpd00082_e0';... % D-fructose
-%     'EX_cpd00076_e0';... % Sucrose
